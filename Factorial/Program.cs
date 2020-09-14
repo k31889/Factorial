56150870f570307d98cae7e94aba1bc6e2435046
Samuel Woodhams <K31889@eltham-college.org.uk>
2020-09-14 11:30:37 +0100

    Add project files.

---
+++ Factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int Fact;
+            Console.WriteLine("Enter the number");
+            x = Convert.ToInt32(Console.ReadLine());
+            Fact = x;
+            for (int i = 1; i < x; i++)
+            {
+                Fact = Fact * (x - i);
+            }
+            Console.WriteLine(Fact);
+        }
+    }
+}
